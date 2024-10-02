47da5524bdd3d76dd14b76254ac227737500b47c
nguyen1oc <131537455+nguyen1oc@users.noreply.github.com>
2024-10-02 21:41:33 +0700

    Update recursive.h

--- Special List/recursive.h
+++ Special List/recursive.h
@@ -1,15 +1,18 @@
-bool recursive(const char*s, AStack<char>& in){
-    if(*s == '\0') return in.length() == 0;
-    if(*s == '(') in.push (*s);
-    if(*s == ')'){
-        if(in.length() == 0) return false;
-        else in.pop();
+bool recursive(const char* s, Stack<char>& in) {
+    if (*s == '\0') return in.length() == 0;  
+
+    if (*s == '(') in.push(*s); 
+
+    if (*s == ')'){
+        if (in.length() == 0) return false;  
+        else in.pop(); 
     }
-    return recursive(s + 1, in);
+    return recursive(s + 1, in); 
 }
+
 bool recursiveParentheses(const string &L) {
-    /* to check if the string L includes balanced parentheses or not */
-    AStack<char>in;
-    return recursive(&L[0], in);
+    Stack<char>* in = new AStack<char>();  
+    bool result = recursive(&L[0], *in); 
+    delete in;  
+    return result;
 }
-    
