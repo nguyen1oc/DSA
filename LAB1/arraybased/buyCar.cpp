dc24e3ba4645de915dc3e6d3b0ccf2caddfe677c
nguyen1oc <131537455+nguyen1oc@users.noreply.github.com>
2024-10-03 22:44:23 +0700

    Create buyCar.cpp

---
+++ LAB1/arraybased/buyCar.cpp
@@ -0,0 +1,10 @@
+int buyCar(int* nums, int length, int k) {
+    sort (nums, nums + length);
+    int count = 0;
+    while (count < length){
+         if (nums[count] <= k)
+            k -= nums[count++];
+        else break;
+    }
+    return count;
+}
