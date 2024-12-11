08446d8d54702ebe099b517a906ec27e2ea06006
nguyen1oc <131537455+nguyen1oc@users.noreply.github.com>
2024-12-12 01:21:37 +0700

    Update DGraphModel.h

--- DGraphModel.h
+++ DGraphModel.h
@@ -1,7 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   DGraphModel.h
+ * Author: LTSACH
+ *
+ * Created on 23 August 2020, 19:36
+ */
+
 #ifndef DGRAPHMODEL_H
 #define DGRAPHMODEL_H
 #include "graph/AbstractGraph.h"
-
+#include "stacknqueue/Queue.h"
+#include "stacknqueue/Stack.h"
+#include "hash/xMap.h"
+#include "sorting/DLinkedListSE.h"
 
 //////////////////////////////////////////////////////////////////////
 ///////////// GraphModel: Directed Graph Model    ////////////////////
